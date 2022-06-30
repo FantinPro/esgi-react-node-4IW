9714cf6e9669b87df111c064f8ad97da35697976
Fantin <fantin@malou.io>
2022-06-30 12:31:43 +0200

    improve error handler

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,7 +1,17 @@
+const { ValidationError } = require('sequelize');
 const config = require('../config/config');
 const { logger } = require('../config/logger');
 
+const formatError = (validationError) => validationError.errors.reduce((acc, error) => {
+    acc[error.path] = error.message;
+    return acc;
+}, {});
+
 const errorHandler = (err, req, res, next) => {
+    if (err instanceof ValidationError) {
+        return res.status(422).json(formatError(err));
+    }
+
     const { statusCode, message } = err;
 
     const response = {
@@ -10,7 +20,7 @@ const errorHandler = (err, req, res, next) => {
         stack: err.stack,
     };
 
-    if (config.env === 'development') {
+    if (config.env === 'local') {
         logger.error(err);
     }
 
